e52533d1a512b84c3908728ae41b6eb45475208a
Mischov <mmischov@gmail.com>
2015-06-17 12:29:48 -0500

    Changed filter to regex and added file output and formatting among other changes.

--- src/mischov/boot_test.clj
+++ src/mischov/boot_test.clj
@@ -12,19 +12,60 @@
   (doto fresh-pod
     (pod/with-eval-in
      (require '[clojure.test :as t]
+              '[clojure.test.junit :as tj]
               '[clojure.java.io :as io]
               '[pjstadig.humane-test-output :refer [activate!]]
-              '[clojure.tools.namespace.find :refer [find-namespaces-in-dir]])
+              '[clojure.tools.namespace.parse :as namespace-parse]
+              '[clojure.tools.namespace.find :as namespace-find])
      (activate!)
-     (defn all-ns* [& dirs]
-       (mapcat #(find-namespaces-in-dir (io/file %)) dirs))
-     (defn test-ns* [pred ns]
-       (binding [t/*report-counters* (ref t/*initial-report-counters*)]
-         (let [ns-obj (the-ns ns)]
-           (t/do-report {:type :begin-test-ns :ns ns-obj})
-           (t/test-vars (filter pred (vals (ns-publics ns))))
-           (t/do-report {:type :end-test-ns :ns ns-obj}))
-         @t/*report-counters*)))))
+     
+     ;; Fix for finding cljc namespaces.
+     (defn read-ns-decl [rdr]
+       (try
+         (loop []
+           (let [form (doto (read {:read-cond :allow} rdr) str)]
+             (if (namespace-parse/ns-decl? form)
+               form
+               (recur))))
+         (catch Exception e nil)))
+     
+     (defn read-file-ns-decl [file]
+       (with-open [rdr (java.io.PushbackReader. (io/reader file))]
+         (read-ns-decl rdr)))
+     
+     (defn find-ns-decls-in-dir [^java.io.File dir]
+       (keep read-file-ns-decl (namespace-find/find-clojure-sources-in-dir dir)))
+     
+     (defn find-namespaces-in-dir [^java.io.File dir]
+       (map second (find-ns-decls-in-dir dir)))
+     
+     ;; Get all namespaces.
+     (defn get-all-ns [& dirs]
+       (-> (mapcat #(find-namespaces-in-dir (io/file %)) dirs)))
+     
+     ;; Narrow down namespaces.
+     (defn filter-namespaces
+       [namespaces regex]
+       (if regex
+         (filter #(re-find regex (str %)) namespaces)
+         namespaces))
+     
+     ;; Run tests.
+     (defn test-with-formatting
+       [namespaces formatter]
+       (case formatter
+         :junit (tj/with-junit-output
+                  (apply t/run-tests namespaces))
+         (apply t/run-tests namespaces)))
+     
+     (defn run-tests
+       [namespaces output-path formatter]
+       (if output-path
+         (with-open [writer (io/writer output-path)]
+           (binding [t/*test-out* writer]
+             (test-with-formatting namespaces formatter)))
+         (test-with-formatting namespaces formatter))))))
+
 
 ;;; This prevents a name collision WARNING between the test task and
 ;;; clojure.core/test, a function that nobody really uses or cares
@@ -32,37 +73,32 @@
 (if ((loaded-libs) 'boot.user)
   (ns-unmap 'boot.user 'test))
 
+
 (core/deftask test
-  "Run clojure.test tests in a pod.
-
-  The --namespaces option specifies the namespaces to test. The default is to
-  run tests in all namespaces found in the project.
-
-  The --filters option specifies Clojure expressions that are evaluated with %
-  bound to a Var in a namespace under test. All must evaluate to true for a Var
-  to be considered for testing by clojure.test/test-vars."
-
+  "Run clojure.test tests in a pod."
   [n namespaces NAMESPACE #{sym} "The set of namespace symbols to run tests in."
-   f filters    EXPR      #{edn} "The set of expressions to use to filter namespaces."]
+   r regex REGEX regex "A regex for filtering namespaces."
+   o output-path PATH str "A string representing the filepath to output test results to. Defaults to *out*."
+   f formatter FORMATTER kw "Tag defining formatter to use with test results. Currently accepts `junit`. Defaults to standard clojure.test output."]
 
   (let [worker-pods (pod/pod-pool (update-in (core/get-env) [:dependencies] into pod-deps) :init init)]
     (core/cleanup (worker-pods :shutdown))
     (core/with-pre-wrap fileset
+      (println "Starting test...")
       (let [worker-pod (worker-pods :refresh)
             namespaces (or (seq namespaces)
-                           (pod/with-eval-in worker-pod
-                             (all-ns* ~@(->> fileset
-                                             core/input-dirs
-                                             (map (memfn getPath))))))]
+                               (pod/with-eval-in worker-pod
+                                 (-> (get-all-ns ~@(->> fileset
+                                                        core/input-dirs
+                                                        (map (memfn getPath))))
+                                     (filter-namespaces ~regex))))]
         (if (seq namespaces)
-          (let [filterf `(~'fn [~'%] (and ~@filters))
-                summary (pod/with-eval-in worker-pod
+          (let [summary (pod/with-eval-in worker-pod
                           (doseq [ns '~namespaces] (require ns))
-                          (let [ns-results (map (partial test-ns* ~filterf) '~namespaces)]
-                            (-> (reduce (partial merge-with +) ns-results)
-                                (assoc :type :summary)
-                                (doto t/do-report))))]
+                          (run-tests '~namespaces ~output-path ~formatter))]
+            (println "Test complete.")
+            (println "Test summary: " (dissoc summary :type))
             (when (> (apply + (map summary [:fail :error])) 0)
-              (throw (ex-info "Some tests failed or errored" summary))))
+              (System/exit 1)))
           (println "No namespaces were tested."))
         fileset))))
